746dbcab8ab620377d4def948c6cb22d904bc999
vmdln <110718729+vmdln@users.noreply.github.com>
2024-05-14 21:48:01 +0000

    Get rid of useless clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 #![warn(clippy::pedantic)]
 #![deny(rust_2018_idioms, unused, future_incompatible, nonstandard_style)]
-#![allow(clippy::module_name_repetitions, clippy::module_inception)]
 
 mod error;
 mod hex_string;
