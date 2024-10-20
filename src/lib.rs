e821a70f17009aae2225f4dfa983602e6f03c23b
vmdln <110718729+vmdln@users.noreply.github.com>
2024-10-20 15:42:25 +0200

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,31 @@
 #![warn(clippy::pedantic)]
 #![deny(rust_2018_idioms, unused, future_incompatible, nonstandard_style)]
 
-//! Handle and parse hex strings of specific length.
+//! Handle and parse hex strings of constant and variable lengths
 //!
+//! ## Example:
 //! Example hex string, an md5 of an empty file:
 //! ```text
 //! d41d8cd98f00b204e9800998ecf8427e
 //! ```
-//!
-//! ## Example:
 //! ```
-//! use hex_str::HexStringN;
+//! use hex_str::{HexString, HexStringN};
 //!
 //! let s = "d41d8cd98f00b204e9800998ecf8427e";
-//! let v = HexStringN::<16>::try_parse(s).unwrap();
 //!
+//! // constant length, encoded in the type system
+//! let u = HexStringN::<16>::try_parse(s).unwrap();
+//! assert_eq!(u, "d41d8cd98f00b204e9800998ecf8427e");
+//!
+//! // variable length
+//! let v = HexString::try_parse(s).unwrap();
 //! assert_eq!(v, "d41d8cd98f00b204e9800998ecf8427e");
 //! ```
 
 //! ## Feature flags:
-//! - `serde` - adds the ability to serialize and deserialize a [`HexString`] using `serde`.
+//! - `serde` - adds the ability to serialize, and deserialize [`HexString`]'s, and [`HexStringN`]'s using `serde`.
 //! - `rand` - adds implementation of `rand`'s [`Standard`](https://docs.rs/rand/0.8.4/rand/distributions/struct.Standard.html)
-//!   distribution, which enables random generation of [`HexString`]'s directly.
+//!   distribution, which enables random generation of [`HexStringN`]'s directly.
 //!
 //! #### Using `serde` feature:
 //! ```
